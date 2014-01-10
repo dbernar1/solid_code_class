c9d865362a656bb4155f60ac4db2481100959472
Dan Bernardic <dbernardic@farmmedia.com>
2014-01-10 13:08:11 -0600

    Removed some of the duplication.

--- triangle/taylorstapleton/test.py
+++ triangle/taylorstapleton/test.py
@@ -3,29 +3,18 @@
 import subprocess
 
 def testIso(debug):
+	test_cases = { 'isosceles acute': [ '0' , '0' , '2' , '0', '1' , '4' ], 'isosceles obtuse': [ '0' , '0' , '4' , '0', '2' , '1' ], 'isosceles right': [ '0' , '0' , '2' , '0', '1' , '1' ] }
+
 	toReturn = ""
-        output = subprocess.check_output(['./triangle' , '0' , '0' , '2' , '0', '1' , '4'])
-	if(debug == "true"):
-		print("output = " + output)
-		print("expected = isosceles acute    actual = " + output)
-	if(output != "isosceles acute\n"):
-		toReturn += "acute "
 
+	for expected_output, provided_input in test_cases.iteritems():
+		output = subprocess.check_output( [ './triangle' ] + provided_input )
+		if ( debug == "true" ):
+			print("output = " + output)
+			print( "expected = " + expected_output + "    actual = " + output )
 
-	output = subprocess.check_output(['./triangle' , '0' , '0' , '4' , '0', '2' , '1'])
-        if(debug == "true"):
-                print("output = " + output)
-                print("expected = isosceles obtuse    actual = " + output)
-        if(output != "isosceles obtuse\n"):
-                toReturn += "obtuse "
-
-
-	output = subprocess.check_output(['./triangle' , '0' , '0' , '2' , '0', '1' , '1'])
-        if(debug == "true"):
-                print("output = " + output)
-                print("expected = isosceles right    actual = " + output)
-        if(output != "isosceles right\n"):
-                toReturn += "right "
+		if ( output != expected_output+'\n' ):
+			toReturn += expected_output.split()[1] + " "
 
 	return toReturn
 
